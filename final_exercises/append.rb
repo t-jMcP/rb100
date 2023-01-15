bb4a34004f180d2dea6537bad30aa4c1b4bf853c
Tom McPherson <tom.mcpherson@outlook.com>
2023-01-15 12:51:14 +0000

    add final exercises

---
+++ final_exercises/append.rb
@@ -0,0 +1,21 @@
+array = [1, 2, 3, 4 , 5, 6, 7, 8, 9, 10]
+
+# Push 11 to array
+array.push(11)
+p array
+
+# Prepend 0 to array
+array.unshift(0)
+p array
+
+# Remove 11 from array
+array.pop
+p array
+
+# Append 3 to array
+array.push(3)
+p array
+
+# Remove duplicates
+array.uniq!
+p array
---
+++ final_exercises/hash_creation.rb
@@ -0,0 +1,10 @@
+schemes1 = {
+  :uk => "Bacs"
+}
+
+schemes2 = {
+  uk: "Bacs"
+}
+
+puts schemes1
+puts schemes2
